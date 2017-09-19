68327ec2485a3e2293f446d4e53f84d1f02709f8
Brian Cortes <brian@monoku.com>
2017-09-19 18:26:55 -0500

    add  form

--- client/main.js
+++ client/main.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router'
 import '../imports/startup/accounts-config.js';
 
-import App from '../imports/ui/App.jsx';
- 
+import Login from '../imports/ui/login.jsx'
+import App from '../imports/ui/App.jsx'
+
 Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
+  render(
+    <Router history={ browserHistory }>
+      <Route path="/" component={ Login } />
+      <Route path="/home" component={ App } />
+    </Router>, 
+    document.getElementById('render-target')
+  );
 });
